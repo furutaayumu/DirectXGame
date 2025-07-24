479f55f905eb470c04d479c26ac5b3d826715aaa
furutaayumu <k023g0086@g.neec.ac.jp>
2025-07-24 10:26:10 +0900

    fin

--- DirectXGame/GameScene.cpp
+++ DirectXGame/GameScene.cpp
@@ -1,20 +1,41 @@
 #include "GameScene.h"
-
+#include <cmath> // sin関数に必要
 using namespace KamataEngine;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { delete sprite_; }
+GameScene::~GameScene() {
+	// デストラクタ
+	delete sprite_;
+}
 
-void GameScene::Initialize() { 
-	// ファイル名を指定してテクスチャを読み込む
+void GameScene::Initialize() {
+
+	//ファイル名を指定してテクスチャを読み込む
 	textureHandle_ = TextureManager::Load("Title.png");
-
+	textureHandle_ = TextureManager::Load("Stage.png");
 	// スプライトインスタンスの生成
 	sprite_ = Sprite::Create(textureHandle_, {0, 0});
+
+	stage_ = new Stage();
+	stage_->Initialize(textureHandle_);
+	
 }
+
+// メンバー変数に追加
 int frameCount = 0;
+
+
+
 void GameScene::Update() {
+	//スプライトの今の座標を取得
+	//Vector2 position = sprite_->GetPosition();
+	////座標を｛２，１｝移動
+	//position.x += 2.0f;
+	//position.y += 1.0f;
+	////移動した座標をスプライトに反映
+	//sprite_->SetPosition(position);
+
 	frameCount++;
 
 	// sin波で上下に揺れるY座標を作る（±10ピクセル範囲で動かす）
@@ -22,20 +43,26 @@ void GameScene::Update() {
 
 	// スプライトの位置を更新
 	sprite_->SetPosition({0.0f, y});
+
+	stage_->Update();
+
 }
 
-void GameScene::Draw() { 
+void GameScene::Draw() 
+{
+	//DirectXCommonインスタンスの取得
 	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
-
-	// スプライト描画前処理
+	//スプライト描画前処理
 	Sprite::PreDraw(dxCommon->GetCommandList());
+	
+	//スプライトインスタンスの描画処理
+	//if(frameCount % 60 >= 30) 
+	//{
+	//	sprite_->Draw();
+	//}
+	
+	stage_->Draw();
 
-	// スプライトインスタンスの描画処理
-
-		sprite_->Draw();
-
-
-	// スプライト描画後処理
+	//スプライト描画後処理
 	Sprite::PostDraw();
 }
-
--- DirectXGame/GameScene.h
+++ DirectXGame/GameScene.h
@@ -1,20 +1,30 @@
 #pragma once
-#include "KamataEngine.h"
-#include "Particle.h"
- class GameScene {
+#include"KamataEngine.h"
+#include "Stage.h"
+class GameScene 
+{
 public:
-
+	/// コンストクラタ
+	/// </summary>
 	GameScene();
-
+	/// <summary>
+	/// デストラクタ
+	/// </summary>
 	~GameScene();
-
+	//初期化
 	void Initialize();
-
+	//更新
 	void Update();
 
+	//描画
 	void Draw();
-
 private:
+	// テクスチャハンドル
 	uint32_t textureHandle_ = 0;
+
+	//スプライト
 	KamataEngine::Sprite* sprite_ = nullptr;
- };
+
+	//ステージ
+	Stage* stage_ = nullptr;
+};
